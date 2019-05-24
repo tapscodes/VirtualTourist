a1a259b4d593f27c1afd6612df0892850cb0d125
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-05-24 15:08:06 -0400

    Added a base xcode project with the proper title

---
+++ VirtualTourist/VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Tristan Pudell-Spatscheck on 5/24/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

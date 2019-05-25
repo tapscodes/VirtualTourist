edd8546451ef82fac281c2368bdfbea5414fa4d4
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-05-25 16:53:50 -0400

    Setup Storyboard File
    Set up the storyboard file for virtualtourist.

---
+++ VirtualTourist/VirtualTourist/View Controllers/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Tristan Pudell-Spatscheck on 5/25/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import UIKit
+class MapViewController: UIViewController, MKMapViewDelegate{
+    @IBOutlet weak var mapView: MKMapView!
+    
+}
---
+++ VirtualTourist/VirtualTourist/View Controllers/PhotoGridViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoGridViewController.swift
+//  VirtualTourist
+//
+//  Created by Tristan Pudell-Spatscheck on 5/25/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class PhotoGridViewController: UICollectionView{
+    
+}

b375fa3b53aa1fe9feb5f484e7f6d804d0a6d4b6
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-06-01 08:54:59 -0400

    Made Pins Persistant

--- VirtualTourist/VirtualTourist/View Controllers/MapViewController.swift
+++ VirtualTourist/VirtualTourist/View Controllers/MapViewController.swift
@@ -8,24 +8,48 @@
 import Foundation
 import MapKit
 import UIKit
+import CoreData
 class MapViewController: UIViewController, MKMapViewDelegate{
     @IBOutlet weak var deleteWarning: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     //function called when view is loaded
+    //fetch stored pins
+    func fetchPins () {
+        let fetchRequest: NSFetchRequest<Pin> = Pin.fetchRequest()
+        do {
+            // get the data
+            let result = try dataController.viewContext.fetch(fetchRequest)
+            pins=result
+            // put it on the map
+            var annotations = [MKPointAnnotation()]
+            for pin in pins {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2D(latitude: pin.lat, longitude: pin.long)
+                annotations.append(annotation)
+            }
+            self.mapView.addAnnotations(annotations)
+        } catch {
+            // XXX add error pop up here
+        }
+    }
+    //function called when the view loads
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mapView.isUserInteractionEnabled = true
         deleteWarning.isUserInteractionEnabled = false
         deleteWarning.isHidden = true
         mapView.delegate = self
+        // loading the data controller so it associates with storage
+        dataController.load()
+        fetchPins()
         //adds long press scanner
         let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
         mapView.addGestureRecognizer(longTapGesture)
         //TEST VALUES FOR PINS+ANNOTATIONS (IN NYC)
         //tests zoom used in photogrid
         //locationZoom(with: CLLocationCoordinate2D(latitude: pins[0].lat, longitude: pins[0].long))
-        APICommands().getPhotos(lat: 40, long: -74)
-        APICommands().requestImage(farm: "6", secret: "8b816d7d81", ID: "20875765031", server: "5675")
+        //APICommands().getPhotos(lat: 40, long: -74)
+        //APICommands().requestImage(farm: "6", secret: "8b816d7d81", ID: "20875765031", server: "5675")
     }
     //zooms in on a location, used in PhotoGridVC
     func locationZoom(with coordinate: CLLocationCoordinate2D){
@@ -33,37 +57,59 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         self.mapView.isUserInteractionEnabled = false
         self.mapView.setRegion(region, animated: true)
     }
+    //sets up pins on mapview
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let reuseId = "pin"
-            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-            if pinView == nil {
-                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-                pinView!.canShowCallout = false
-                pinView!.pinTintColor = .red
-            }
-            else {
-                pinView!.annotation = annotation
-            }
-            return pinView
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
         }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
     //functionc alled when pin is tapped
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        var pinIndex: Int = -1
         for (index,pin) in pins.enumerated() {
             if(pin.lat == view.annotation!.coordinate.latitude) && (pin.long == view.annotation!.coordinate.longitude) {
-                print ("Got it")
-                print (index)
+                pinIndex = index
+                break
             }
         }
-        //ADD PRESENT LATER (currently crashes app)
-        //let vc =  UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detailVC")
-        //self.present(vc, animated: true)
+        if (pinIndex == -1 ) { return }
+        if (deleteWarning.isHidden == true ) {
+            print("drill down: ", pinIndex)
+            // go to photos
+            //ADD PRESENT LATER (currently crashes app)
+            //let vc =  UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detailVC")
+            //self.present(vc, animated: true)
+            
+        } else {
+            // delete pin
+            let rmPin: Pin
+            print("delete pin: ", pinIndex)
+            rmPin = pins[pinIndex]
+            pins.remove(at: pinIndex) // remove from array
+            do { // remove it from persisten storage and save.
+                // This will also delete all photos in persistent storage based on the data model
+                dataController.viewContext.delete(rmPin)
+                try dataController.viewContext.save()
+            } catch {
+                // add an error if we can't delete and save
+            }
+            self.mapView.removeAnnotation(view.annotation!) // remove from view
+        }
     }
     //func called when edit is tapped
     @IBAction func editTapped(_ sender: Any) {
         if(deleteWarning.isHidden == true){
-        deleteWarning.isHidden = false
+            deleteWarning.isHidden = false
         }else{
-        deleteWarning.isHidden = true
+            deleteWarning.isHidden = true
         }
     }
     //whats happens when a long hold is detected
@@ -80,6 +126,11 @@ class MapViewController: UIViewController, MKMapViewDelegate{
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: pin.lat, longitude: pin.long)
             self.mapView.addAnnotation(annotation)
+            do {
+                try dataController.viewContext.save()
+            } catch {
+                // XXX DO SOMETHING WHEN WE CAN't SAVE
+            }
         }
     }
 }
